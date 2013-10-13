a97d7ab97f15a94af090fadc013fbe84a9bb8977
Eliot Arntz <arntzel@gmail.com>
2013-10-14 00:53:19 +0200

    first commit

---
+++ Out of this World/OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Eliot Arntz on 10/10/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
---
+++ Out of this World/OWOuterSpaceTableViewController.m
@@ -0,0 +1,164 @@
+//
+//  OWOuterSpaceTableViewController.m
+//  Out of this World
+//
+//  Created by Eliot Arntz on 10/10/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "OWOuterSpaceTableViewController.h"
+
+@interface OWOuterSpaceTableViewController ()
+
+@end
+
+@implementation OWOuterSpaceTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    /* We first create our NSString objects with our planet names */
+    NSString *planet1 = @"Mercury";
+    NSString *planet2 = @"Venus";
+    NSString *planet3 = @"Earth";
+    NSString *planet4 = @"Mars";
+    NSString *planet5 = @"Jupiter";
+    NSString *planet6 = @"Saturn";
+    NSString *planet7 = @"Uranus";
+    NSString *planet8 = @"Neptune";
+
+    /* We initialize our NSMutableArray. */
+    self.planets = [[NSMutableArray alloc] init];
+    
+    /* We add our NSString objects to our planets array */
+    [self.planets addObject:planet1];
+    [self.planets addObject:planet2];
+    [self.planets addObject:planet3];
+    [self.planets addObject:planet4];
+    [self.planets addObject:planet5];
+    [self.planets addObject:planet6];
+    [self.planets addObject:planet7];
+    [self.planets addObject:planet8];
+    
+    
+    
+//    NSMutableDictionary *myDictionary = [[NSMutableDictionary alloc] init];
+//    NSString *firstColor = @"red";
+//    [myDictionary setObject:firstColor forKey:@"firetruck color"];
+//    [myDictionary setObject:@"blue" forKey:@"ocean color"];
+//    [myDictionary setObject:@"yellow" forKey:@"star color"];
+//    NSLog(@"%@", myDictionary);
+    
+//    NSString *blueString = [myDictionary objectForKey:@"ocean color"];
+//    NSLog(@"%@",blueString);
+    
+//    NSNumber *myNumber = [NSNumber numberWithInt:5];
+//    NSLog(@"%@", myNumber);
+//    NSNumber *floatNumber = [NSNumber numberWithFloat:3.14];
+//    NSLog(@"%@", floatNumber);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+/* Changing the return value alters the number of sections in our tableView */
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+/* Changing the return value alters the number of rows in our tableView. We can return a dynamic number of rows in our tableView based on the number of elements in our planets array. */
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return [self.planets count];
+}
+
+/* We return a UITableViewCell which will be displayed on our TableView. After the comment configure cell we update the textLabel's text property by accessing the string elements in our planets array. */
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = [self.planets objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
